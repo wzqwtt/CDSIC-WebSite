94b4b4a5d48c83bdff539a6137bc9cc85628d73f
lesileqin <1722249371@qq.com>
2022-02-09 15:42:19 +0800

    创建项目初始结构

---
+++ cdsic-front/src/main/java/com/cdsic/front/CdsicFrontApplication.java
@@ -0,0 +1,13 @@
+package com.cdsic.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdsicFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdsicFrontApplication.class, args);
+    }
+
+}
