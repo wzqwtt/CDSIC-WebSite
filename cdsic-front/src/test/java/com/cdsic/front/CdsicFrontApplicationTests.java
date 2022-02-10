fdd049a76386d740dc2c0cdf719138b61f7b3243
lesileqin <1722249371@qq.com>
2022-02-10 20:38:54 +0800

    测试逆向工程功能

--- cdsic-front/src/test/java/com/cdsic/front/CdsicFrontApplicationTests.java
+++ cdsic-front/src/test/java/com/cdsic/front/CdsicFrontApplicationTests.java
@@ -1,13 +1,31 @@
 package com.cdsic.front;
 
+import com.cdsic.front.entity.ProjectEntity;
+import com.cdsic.front.service.ProjectService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CdsicFrontApplicationTests {
+    @Autowired
+    ProjectService projectService;
 
     @Test
     void contextLoads() {
+
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setPtitle("abc");
+        projectEntity.setPcontent("abc");
+        projectEntity.setPimage("abc");
+        projectEntity.setPplatform("abc");
+        projectEntity.setPperson("abc");
+        projectEntity.setPsynopsis("abc");
+
+        boolean save = projectService.save(projectEntity);
+        if(save){
+            System.out.println("保存成功！！");
+        }
     }
 
 }
