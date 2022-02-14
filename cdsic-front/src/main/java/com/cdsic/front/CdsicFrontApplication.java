14a4382c27033b8072d290396506e69c96b81aaa
lesileqin <1722249371@qq.com>
2022-02-14 14:36:37 +0800

    引入Nacos注册中心

--- cdsic-front/src/main/java/com/cdsic/front/CdsicFrontApplication.java
+++ cdsic-front/src/main/java/com/cdsic/front/CdsicFrontApplication.java
@@ -3,9 +3,11 @@ package com.cdsic.front;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.cdsic.front.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CdsicFrontApplication {
 
     public static void main(String[] args) {
