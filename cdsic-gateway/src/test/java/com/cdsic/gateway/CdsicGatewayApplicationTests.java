809ff953d2b23ca2cfd7252845c75d591455c011
lesileqin <1722249371@qq.com>
2022-02-14 15:47:17 +0800

    加入Gateway服务并测试

---
+++ cdsic-gateway/src/test/java/com/cdsic/gateway/CdsicGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdsic.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdsicGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
