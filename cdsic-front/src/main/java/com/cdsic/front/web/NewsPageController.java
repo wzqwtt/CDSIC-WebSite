7a2d20897593c7ce3156b08f831070c7974bac77
lesileqin <1722249371@qq.com>
2022-02-18 20:55:44 +0800

    新闻页Controller

---
+++ cdsic-front/src/main/java/com/cdsic/front/web/NewsPageController.java
@@ -0,0 +1,50 @@
+package com.cdsic.front.web;
+
+import com.cdsic.front.entity.NewsEntity;
+import com.cdsic.front.entity.ProjectEntity;
+import com.cdsic.front.service.NewsService;
+import com.cdsic.front.service.ProjectService;
+import org.apache.commons.lang.time.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class NewsPageController {
+
+    @Autowired
+    NewsService newsService;
+
+    @Autowired
+    ProjectService projectService;
+
+    @GetMapping("/news")
+    public String newsPage(Model model) {
+
+        // 获取所有新闻
+        List<NewsEntity> NewsEntities = newsService.getNewsOrderDescByDate();
+
+        model.addAttribute("news", NewsEntities);
+
+        // 获取一个月前的时间
+        Date now = new Date();
+        Date startDate = DateUtils.addDays(now, -30);
+        model.addAttribute("time", startDate);
+
+        // 获取最近六条新闻数据
+        List<ProjectEntity> projectEntities = projectService.getRecentProject();
+        model.addAttribute("project", projectEntities);
+
+        return "news";
+    }
+
+//    @GetMapping("/news/")
+//    public String singleNewsPage(Model model) {
+//
+//    }
+
+}
